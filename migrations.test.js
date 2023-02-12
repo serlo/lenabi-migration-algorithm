3df09e2bf282c10629bdddb4450dfe3582d10d25
Stephan Kulla <git.mail@kulla.me>
2023-02-12 18:10:57 +0100

    test: Add migration v1 -> v4

--- migrations.test.js
+++ migrations.test.js
@@ -71,3 +71,46 @@ test('Migration from v1 to v2 adds caption to "sidebyside" plugin', () => {
     version: 4,
   })
 })
+
+test('Migration from v1 to v4 would apply all migrations', () => {
+  expect(
+    applyMigrations({
+      document: {
+        type: 'https://serlo.org/editor',
+        content: {
+          plugin: 'article',
+          state: [
+            {
+              plugin: 'image',
+              state: { src: 'http://example.com/image.png' },
+            },
+            {
+              plugin: 'multimedia',
+              state: { explanation: 'content 1', multimedia: 'content 2' },
+            },
+          ],
+        },
+        version: 1,
+      },
+    })
+  ).toEqual({
+    type: 'https://serlo.org/editor',
+    content: {
+      plugin: 'article',
+      state: [
+        {
+          plugin: 'image',
+          state: {
+            src: 'http://example.com/image.png',
+            metadata: { author: null, license: null },
+          },
+        },
+        {
+          plugin: 'sidebyside',
+          state: { left: 'content 1', right: 'content 2', caption: '' },
+        },
+      ],
+    },
+    version: 4,
+  })
+})
